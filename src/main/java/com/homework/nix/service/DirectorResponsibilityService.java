565da424e922987f9985675927a9e61e79e53faa
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-06-29 15:16:40 +0300

    Change method name

--- src/main/java/com/homework/nix/service/DirectorResponsibilityService.java
+++ src/main/java/com/homework/nix/service/DirectorResponsibilityService.java
@@ -4,7 +4,7 @@ import com.homework.nix.data.Group;
 
 public class DirectorResponsibilityService {
 
-    public void allResponsibility(Group group){
+    public void secondaryResponsibility(Group group){
         System.out.println("Помогает команде.");
         System.out.println("Мотивирует команду.");
         System.out.println("Отвечает на вопросы команды.");
