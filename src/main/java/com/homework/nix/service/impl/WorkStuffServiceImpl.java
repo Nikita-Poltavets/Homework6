f28a4690a49de57f884c09afc63aa0f335c24d71
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-06-30 16:52:43 +0300

    Create Value annotation

--- src/main/java/com/homework/nix/service/impl/WorkStuffServiceImpl.java
+++ src/main/java/com/homework/nix/service/impl/WorkStuffServiceImpl.java
@@ -1,10 +1,11 @@
 package com.homework.nix.service.impl;
 
+import com.homework.nix.annotation.Value;
 import com.homework.nix.service.WorkStuffService;
 
 public class WorkStuffServiceImpl implements WorkStuffService {
 
-
+    @Value
     private String briefcase;
 
     @Override
