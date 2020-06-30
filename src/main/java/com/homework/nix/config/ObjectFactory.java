81ac989b5727232150883634ad2fe1e6f9fc99a9
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-06-30 17:25:58 +0300

    Add opportunity to get information from application.properties

--- src/main/java/com/homework/nix/config/ObjectFactory.java
+++ src/main/java/com/homework/nix/config/ObjectFactory.java
@@ -1,9 +1,12 @@
 package com.homework.nix.config;
 
+import com.homework.nix.annotation.Value;
 import com.homework.nix.config.impl.JavaApplicationConfiguration;
 import com.homework.nix.service.VehicleService;
 import com.homework.nix.service.impl.WorkProcessServiceImpl;
+import com.homework.nix.util.ResourceUtil;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,10 +37,27 @@ public class ObjectFactory {
         T t;
         try {
             t = implClass.getDeclaredConstructor().newInstance();
-
+            initFields(implClass, t);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException("невозможно создать класс: " + e.getClass().getName() + " " + ",msg: " + e.getMessage());
         }
         return t;
+    }
+
+
+    private <T> void initFields(Class<? extends T> implClass, T t) {
+        Map<String, String> map = ResourceUtil.getResource("application.properties");
+        for (Field field : implClass.getDeclaredFields()) {
+            Value annotation = field.getAnnotation(Value.class);
+            if (annotation != null) {
+                String value = annotation.value().isEmpty() ? map.get(field.getName()) : annotation.value();
+                field.setAccessible(true);
+                try {
+                    field.set(t, value);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Возникли проблемы с инициализацией филда: " + e.getMessage());
+                }
+            }
+        }
     }
 }
