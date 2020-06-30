2b76969e546069d5ded0b9778d72b98f6e9d0857
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-06-30 17:06:01 +0300

    Add WorkStuffService to DirectorResponsibilityServiceImpl

--- src/main/java/com/homework/nix/service/impl/DirectorResponsibilityServiceImpl.java
+++ src/main/java/com/homework/nix/service/impl/DirectorResponsibilityServiceImpl.java
@@ -1,12 +1,17 @@
 package com.homework.nix.service.impl;
 
+import com.homework.nix.config.ObjectFactory;
 import com.homework.nix.data.Group;
 import com.homework.nix.service.DirectorResponsibilityService;
+import com.homework.nix.service.WorkStuffService;
 
 public class DirectorResponsibilityServiceImpl implements DirectorResponsibilityService {
 
+    private WorkStuffService workStuffService = ObjectFactory.getInstance().createObject(WorkStuffService.class);
+
     @Override
     public void secondaryResponsibility(Group group){
+        workStuffService.check();
         System.out.println("Помогает команде.");
         System.out.println("Мотивирует команду.");
         System.out.println("Отвечает на вопросы команды.");
